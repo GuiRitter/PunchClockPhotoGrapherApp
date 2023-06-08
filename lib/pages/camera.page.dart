66c9767a74fdab1070d3164433415c2777b1aa90
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2023-06-08 09:03:14 -0300

    improved loading indicators

--- lib/pages/camera.page.dart
+++ lib/pages/camera.page.dart
@@ -3,9 +3,11 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:punch_clock_photo_grapher_mobile/main.dart';
+import 'package:punch_clock_photo_grapher_mobile/models/on_pressed_step_data.dart';
 import 'package:punch_clock_photo_grapher_mobile/pages/home.page.dart';
 import 'package:punch_clock_photo_grapher_mobile/pages/submit.page.dart';
 import 'package:http/http.dart' as http;
+import 'package:punch_clock_photo_grapher_mobile/widgets/button-with-loading.widget.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 /*
@@ -18,15 +20,8 @@ next button? or tap image
 sign out button
 */
 
-class CameraPage extends StatefulWidget {
+class CameraPage extends StatelessWidget {
   const CameraPage({super.key});
-
-  @override
-  State<CameraPage> createState() => _CameraPageState();
-}
-
-class _CameraPageState extends State<CameraPage> {
-  var _isLoading = false;
 
   @override
   Widget build(BuildContext context) => WillPopScope(
@@ -42,17 +37,17 @@ class _CameraPageState extends State<CameraPage> {
               ],
             ),
             actions: [
-              TextButton(
-                child: _isLoading
-                    ? const CircularProgressIndicator()
-                    : const Icon(
-                        Icons.api,
-                      ),
-                onPressed: () async {
-                  if (_isLoading) {
-                    return;
-                  }
-
+              ButtonWithLoading(
+                buttonBuilder: ({
+                  required VoidCallback onPressed,
+                  required Widget child,
+                }) =>
+                    TextButton(
+                  onPressed: onPressed,
+                  child: child,
+                ),
+                loadingIndicator: const CircularProgressIndicator(),
+                beforeLoading: () async {
                   var prefs = await SharedPreferences.getInstance();
                   var token = prefs.getString(
                     "token",
@@ -66,12 +61,16 @@ class _CameraPageState extends State<CameraPage> {
                         HomePage(),
                       );
                     }
-                    return;
+                    return OnPressedStepData(shouldContinue: false);
                   }
 
-                  setState(() {
-                    _isLoading = true;
-                  });
+                  return OnPressedStepData(
+                    data: {"token": token},
+                    shouldContinue: true,
+                  );
+                },
+                duringLoading: (OnPressedStepData beforeData) async {
+                  var token = beforeData.data!["token"];
 
                   var response = await http.get(
                     Uri.parse(
@@ -82,9 +81,14 @@ class _CameraPageState extends State<CameraPage> {
                     },
                   );
 
-                  setState(() {
-                    _isLoading = false;
-                  });
+                  return OnPressedStepData(
+                    data: {"response": response},
+                    shouldContinue: true,
+                  );
+                },
+                afterLoading: (OnPressedStepData beforeData,
+                    OnPressedStepData duringData) async {
+                  var response = duringData.data!["response"];
 
                   var body = jsonDecode(response.body);
 
@@ -105,6 +109,9 @@ class _CameraPageState extends State<CameraPage> {
                     }
                   }
                 },
+                child: const Icon(
+                  Icons.api,
+                ),
               ),
               TextButton(
                 child: const Icon(
--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -3,10 +3,11 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:punch_clock_photo_grapher_mobile/main.dart';
+import 'package:punch_clock_photo_grapher_mobile/models/on_pressed_step_data.dart';
 import 'package:punch_clock_photo_grapher_mobile/models/sign_in.dart';
 import 'package:punch_clock_photo_grapher_mobile/pages/camera.page.dart';
 import 'package:http/http.dart' as http;
-import 'package:punch_clock_photo_grapher_mobile/widgets/elevated-button-with-loading.widget.dart';
+import 'package:punch_clock_photo_grapher_mobile/widgets/button-with-loading.widget.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatelessWidget {
@@ -25,63 +26,6 @@ class HomePage extends StatelessWidget {
         navigate(context, const CameraPage());
       }
     });
-
-    var signInButton = ElevatedButtonWithLoading(
-        beforeLoading: () async {
-          if ((_formKey.currentState == null) ||
-              (!_formKey.currentState!.validate())) {
-            return;
-          }
-
-          _formKey.currentState!.save();
-        },
-        duringLoading: (Map<String, dynamic>? beforeData) async {
-          var response = await http.post(
-            Uri.parse(
-              "https://guilherme-alan-ritter.net/punch_clock_photo_grapher/api/user/sign_in",
-            ),
-            body: SignIn(
-              userId: _userId!,
-              password: _password!,
-            ).toJson(),
-          );
-
-          return {"response": response};
-        },
-        afterLoading: (Map<String, dynamic>? beforeData,
-            Map<String, dynamic>? duringData) async {
-          var response = duringData!["response"];
-
-          var body = jsonDecode(response.body);
-
-          if (response.statusCode != HttpStatus.ok) {
-            var error = body["error"];
-            var message = "${error ?? "Unknown error."}";
-
-            if (context.mounted) {
-              showSnackBar(context, message);
-            }
-
-            return;
-          }
-
-          var data = body["data"];
-          var token = data["token"];
-
-          var prefs = await SharedPreferences.getInstance();
-          await prefs.setString(
-            "token",
-            token,
-          );
-
-          if (context.mounted) {
-            navigate(
-              context,
-              const CameraPage(),
-            );
-          }
-        },
-        text: "Sign in");
 
     return Scaffold(
       appBar: AppBar(
@@ -125,7 +69,90 @@ class HomePage extends StatelessWidget {
                         : null,
                   ),
                 ),
-                signInButton,
+                ButtonWithLoading(
+                  buttonBuilder: ({
+                    required VoidCallback onPressed,
+                    required Widget child,
+                  }) =>
+                      ElevatedButton(
+                    onPressed: onPressed,
+                    child: child,
+                  ),
+                  loadingIndicator: SizedBox(
+                    height:
+                        Theme.of(context).textTheme.bodySmall?.fontSize ?? 0,
+                    width: Theme.of(context).textTheme.bodySmall?.fontSize ?? 0,
+                    child: CircularProgressIndicator(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
+                  ),
+                  beforeLoading: () async {
+                    if ((_formKey.currentState == null) ||
+                        (!_formKey.currentState!.validate())) {
+                      return OnPressedStepData(
+                        shouldContinue: false,
+                      );
+                    }
+
+                    _formKey.currentState!.save();
+
+                    return OnPressedStepData(
+                      shouldContinue: true,
+                    );
+                  },
+                  duringLoading: (OnPressedStepData beforeData) async {
+                    var response = await http.post(
+                      Uri.parse(
+                        "https://guilherme-alan-ritter.net/punch_clock_photo_grapher/api/user/sign_in",
+                      ),
+                      body: SignIn(
+                        userId: _userId!,
+                        password: _password!,
+                      ).toJson(),
+                    );
+
+                    return OnPressedStepData(
+                      data: {"response": response},
+                      shouldContinue: true,
+                    );
+                  },
+                  afterLoading: (OnPressedStepData beforeData,
+                      OnPressedStepData duringData) async {
+                    var response = duringData.data!["response"];
+
+                    var body = jsonDecode(response.body);
+
+                    if (response.statusCode != HttpStatus.ok) {
+                      var error = body["error"];
+                      var message = "${error ?? "Unknown error."}";
+
+                      if (context.mounted) {
+                        showSnackBar(context, message);
+                      }
+
+                      return;
+                    }
+
+                    var data = body["data"];
+                    var token = data["token"];
+
+                    var prefs = await SharedPreferences.getInstance();
+                    await prefs.setString(
+                      "token",
+                      token,
+                    );
+
+                    if (context.mounted) {
+                      navigate(
+                        context,
+                        const CameraPage(),
+                      );
+                    }
+                  },
+                  child: const Text(
+                    "Sign in",
+                  ),
+                ),
               ],
             ),
           ),
